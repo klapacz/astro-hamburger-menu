32e3e92131f4d6ba4f630d0caf7b8784d82752a9
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-05-06 23:59:21 +0200

    dropdown

--- astro.config.ts
+++ astro.config.ts
@@ -5,4 +5,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({})],
+  vite: {
+    ssr: {
+      // https://github.com/natemoo-re/astro-icon#setup
+      external: ["svgo"],
+    },
+  },
 });
